608dee1b0ee9ac6ed7b6bad6950d22d256c1a746
HMuuse <wharrido@outlook.com>
2024-11-14 11:56:04 +0100

    Object System

--- defend_the_tent/Assets/Scripts/Hassan/BaseObject.cs
+++ defend_the_tent/Assets/Scripts/Hassan/BaseObject.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class BaseObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public ObjectSO objectSO;
+
+    private IObjectParent objectParent;
+
+    private void Update()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetObjectParent(IObjectParent parent)
     {
-        
+        if (this.objectParent != null)
+        {
+            // If the object already has a parent, remove it before setting a new one
+            this.objectParent.ClearObject();
+        }
+        // Grab the transform of the parent that the object should follow
+        Transform parentTransform = parent.GetObjectFollowTransform();
+        transform.parent = parentTransform;
+        transform.localPosition = Vector3.zero;
+        parent.SetObject(this);
+    }
+
+    public GameObject GetObjectParent()
+    {
+        return transform.parent.gameObject;
+    }
+
+    public static BaseObject SpawnObject(ObjectSO objectSO, IObjectParent objectParent)
+    {
+        Transform ObjectTransform = Instantiate(objectSO.objectPrefab);
+
+        // Grab the script attached to the baseObject
+        BaseObject baseObject = ObjectTransform.GetComponent<BaseObject>();
+
+        baseObject.SetObjectParent(objectParent);
+
+        return baseObject;
     }
 }
