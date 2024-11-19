ac8a9d281f50b8c23554eeb7b7c60f15e03e2efd
Beeeq <vanbuurenelias@gmail.com>
2024-11-19 11:32:08 +0100

    add playerinput script

---
+++ defend_the_tent/Assets/Scripts/Elias/Player/PlayerInput.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : Player
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if (moveVector.normalized != Vector3.zero)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isIdle", false);
+        }
+        else if (moveVector.normalized == Vector3.zero)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isIdle", true);
+        }
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputMovement = context.ReadValue<Vector2>();
+        Vector3 localMove = new Vector3(inputMovement.x, 0, inputMovement.y);
+
+        //get the current rotation from the camera and rotate the movement inputs so forward is forward relative to the camera
+        float rotationalOffset = Camera.main.transform.rotation.eulerAngles.y;
+        moveVector = Quaternion.AngleAxis(rotationalOffset, Vector3.up) * localMove;
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        inputJumped = context.action.triggered;
+    }
+    public void OnBuild(InputAction.CallbackContext context)
+    {
+        inputBuilded = context.action.triggered;
+    }
+    public void OnGrab(InputAction.CallbackContext context)
+    {
+        inputGrabStrength = context.ReadValue<float>();
+    }
+}
