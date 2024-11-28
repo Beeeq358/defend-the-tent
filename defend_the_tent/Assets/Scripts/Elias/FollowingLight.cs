041f91c0502dbb710374a1386d2e21f0148ae5fb
Beeeq <vanbuurenelias@gmail.com>
2024-11-28 15:40:29 +0100

    add lighting spotlights

--- defend_the_tent/Assets/Scripts/Elias/FollowingLight.cs
+++ defend_the_tent/Assets/Scripts/Elias/FollowingLight.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class FollowingLight : MonoBehaviour
 {
+    private GameObject parent;
+    public void LogOn(GameObject parentObject)
+    {
+        parent = parentObject;
+    }
 
+
+    private void Update()
+    {
+        if (parent != null)
+        {
+            transform.LookAt(parent.transform.position);
+        }
+    }
 }
--- defend_the_tent/Assets/Scripts/Elias/SpotLight.cs
+++ defend_the_tent/Assets/Scripts/Elias/SpotLight.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 public class SpotLight : MonoBehaviour
 {
     private float waitTime;
+    private GameManager gameManager;
     [SerializeField] private Animator animator;
+    private bool isPrep;
 
     private void Awake()
     {
         transform.Rotate(0, Random.Range(0, 360), 0);
         waitTime = Random.Range(0f, 2f);
-        StartCoroutine(WaitUntilStart());
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (isPrep)
+        {
+            StartCoroutine(WaitUntilStart());
+            isPrep = false;
+        }
+        if (gameManager.gamePhase == GameManager.GamePhase.Preparation)
+            isPrep = true;
     }
 
 
